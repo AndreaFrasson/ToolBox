c36f2db503855f5ab2986a2b56c02282e36cf2db
Andrea Frasson <andrea.frasson.99@gmail.com>
2025-01-09 17:53:19 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,5 +35,5 @@ html_theme = 'sphinx_rtd_theme'
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
 
-matlab_src_dir = os.path.dirname(os.path.abspath(__file__))
+matlab_src_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'matlab')
 primary_domain = 'mat'
